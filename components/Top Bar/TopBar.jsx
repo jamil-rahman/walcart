f866bc7f82fdfcea0e77559d3b9403f74acc9f83
Jamilur Rahman <r.jamilur96@gmail.com>
2022-09-23 04:10:23 +0600

    top section halfway done

--- components/Top Bar/TopBar.jsx
+++ components/Top Bar/TopBar.jsx
@@ -1,6 +1,8 @@
 import { SearchOutlined } from "@ant-design/icons";
 import styles from "../../styles/TopBar.module.css";
-import svg from "../../public/assets/single-01.png"
+import profile from "../../public/assets/single-01.png";
+import favorites from "../../public/assets/favorites.png";
+import cart from "../../public/assets/cart.png";
 import Image from "next/image";
 export default function TopBar() {
   return (
@@ -15,20 +17,35 @@ export default function TopBar() {
             <SearchOutlined />
           </div>
           <input type="text" placeholder="What do you want to order?" />
-           <div className={styles.search_button}>
+          <div className={styles.search_button}>
             <span>Search</span>
-          </div> 
+          </div>
         </div>
       </div>
 
-    {/* Account */}
-    <div className={styles.account_container}>  
-    <Image src={svg} alt="profile_icon"/>
-    </div>
+      {/* Account */}
+      <div className={styles.account_container}>
+        <div style={{ marginRight: "20px" }}>
+          <Image src={profile} alt="profile_icon" />
+        </div>
 
-    {/* Favorites */}
+        <div className={styles.sign_in_container}>
+          <span className={styles.sign_in_button}>Sign In</span>
+          <span className={styles.account_button}>Account</span>
+        </div>
 
-    {/* Cart */}
+        <div className={styles.favorites}>
+          <Image
+            src={favorites}
+            alt="favorites_icon"
+            className={styles.favorite_icon}
+          />
+
+          <div className={styles.cart_icon} style={{ marginLeft: "49px" }}>
+            <Image src={cart} alt="cart_icon" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
