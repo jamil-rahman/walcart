710f1f2467312731f21cd6dd9f814975c1af6bfd
Jamilur Rahman <r.jamilur96@gmail.com>
2022-09-24 09:22:56 +0600

    add favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,6 +9,7 @@ class MyDocument extends Document {
             name="description"
             content="Walcart-Everything Everywhere all at Once!"
           />
+          <link rel="shortcut icon" href="/logo.png" />
           <script
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
